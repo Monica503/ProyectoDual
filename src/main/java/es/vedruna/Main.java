468827822a85354a78307fcdc21f73cf41e33d3c
Monica Liseth Martinez Palacios <dam24-19@vedrunacentroseducativos.onmicrosoft.com>
2025-06-05 20:35:37 +0200

    Proyecto maven ya creado y probado

--- src/main/java/es/vedruna/Main.java
+++ src/main/java/es/vedruna/Main.java
@@ -6,18 +6,5 @@ import static es.vedruna.Positivo.contarPositivos;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int[] numeros = new int[100]; // Asumimos que el usuario no ingresará más de 100 números
-        int numero;
-        int i = 0;
-
-        // Pedimos números hasta que el usuario ingrese 0
-        do {
-            System.out.print("Introduce un numero: ");
-            numero = scan.nextInt();
-            numeros[i] = numero;
-            i++;
-        } while (numero != 0 && i < numeros.length);
-        Positivo.contarPositivos(numeros);
     }
 }
